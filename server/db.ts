53f74f79b8b99225222c718818d28e94554837c5
ErynGalen <115268502+ErynGalen@users.noreply.github.com>
2023-08-18 17:06:51 +0200

    Reuse global ids of players who left
    (this way, when a player dies and respawn, chances are high they will get the same gobal id as before they died)

--- server/db.ts
+++ server/db.ts
@@ -5,32 +5,38 @@ export class Room {
     name: string;
     players: PlayerRef[] = [];
     last_global_id: number = 0;
+    free_global_ids: number[] = [];
     constructor(name: string) {
         this.name = name;
     }
 
     addPlayer(socket: WebSocket, local_id: number, name: string) {
-        this.last_global_id += 1;
-        this.players.push(new PlayerRef(this.last_global_id, local_id, socket, name));
-        socket.send(new m.AssignGlobalID(local_id, this.last_global_id).serialize());
+        let global_id = this.free_global_ids.pop();
+        if (!global_id) {
+            this.last_global_id += 1;
+            global_id = this.last_global_id;
+        }
+        this.players.push(new PlayerRef(global_id, local_id, socket, name));
+        socket.send(new m.AssignGlobalID(local_id, global_id).serialize());
 
         for (let p = 0; p < this.players.length; p++) {
-            if (this.players[p].global_id != this.last_global_id) {
+            if (this.players[p].global_id != global_id) {
                 // inform other player
                 this.players[p].socket.send(new m.PlayerInRoom(this.players[p].local_id,
-                    this.last_global_id, 1, name).serialize());
+                    global_id, 1, name).serialize());
                 // inform new player
                 socket.send(new m.PlayerInRoom(local_id,
                     this.players[p].global_id, 0, this.players[p].name).serialize());
             }
         }
 
-        return this.last_global_id;
+        return global_id;
     }
 
     removePlayer(global_id: number) {
         this.players = this.players.filter(function (p) {
             if (p.global_id == global_id) {
+                this.free_global_ids.push(global_id);
                 return false;
             }
             // inform other player that `global_id` has left
